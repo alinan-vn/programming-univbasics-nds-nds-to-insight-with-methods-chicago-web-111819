0324969396d71263ab172de377af31fb0487d715
Alejandro Linan <jandrolinan@gmail.com>
2019-11-11 16:22:36 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -28,7 +28,8 @@ end
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
-  
+  array = directors_totals(director_data)
+  return array[0]
 end
 
 pp directors_totals(directors_database)
